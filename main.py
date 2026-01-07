5846951e374bcc4c17abe8f8a3cf3cab009f133b
Kaíque Meireles <kaai.que.m@gmail.com>
2026-01-07 04:23:00 -0300

    feat: integrated transformation layer and adjusted pipeline for live API testing

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from concurrent.futures import ThreadPoolExecutor
 import pandas as pd
 from src.get_cep_list import carregar_lista_cep
+from src.data_transformation import normalizar_resultados, validar_dados_transformados
 
 # Teste com mock em caso de bloqueio da API por excesso de requisições
-from tests.test_get_cep_info import consultar_cep_mock as consultar_cep
-# from src.get_cep_info import consultar_cep
+# from tests.test_get_cep_info import consultar_cep_mock as consultar_cep
+from src.get_cep_info import consultar_cep
 
 
-workers = 500  # Valor alto temporário por estar utilizando mock
+# workers = 500  # Valor alto temporário por estar utilizando mock
+workers = 4
 
 with ThreadPoolExecutor(max_workers=workers) as executor:
     print("Carregando lista de CEPs...")
-    lista_cep = carregar_lista_cep(tamanho_amostra=10000)
+    lista_cep = carregar_lista_cep(tamanho_amostra=20)
     
     print("Consultando CEPs...")
     # Convertendo os resultados diretamente para DataFrame
@@ -24,6 +26,9 @@ with ThreadPoolExecutor(max_workers=workers) as executor:
     resultados_erro = resultados_df[resultados_df['status'] == 'erro']
 
     # Salvando os resultados em arquivos CSV
+    resultados_ok = normalizar_resultados(resultados_ok)
+    resultados_ok = validar_dados_transformados(resultados_ok)
     resultados_ok.to_csv('data/output/cep_sucesso.csv', index=False)
+    
     resultados_erro.to_csv('data/output/cep_erro.csv', index=False)
     print("Resultados salvos em 'data/output/'.")
